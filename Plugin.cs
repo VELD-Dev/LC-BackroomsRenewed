51e5d65a8254012fb56be893c94a68f0a1e5fbfc
VELD-Dev <36549174+VELD-Dev@users.noreply.github.com>
2026-01-14 03:24:05 +0100

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VELDDev.BackroomsRenewed;
+
+[BepInPlugin(PluginInfo.GUID, PluginInfo.Version)]
+public class Plugin : BaseUnityPlugin
+{
+    private Harmony harmony = new Harmony(PluginInfo.GUID);
+    private ManualLogSource logger = this.Logger;
+
+    void Start()
+    {
+        logger.Log("""/// B##CKR##MS R#N#W#D \\\""");
+        harmony.PatchAll();
+        logger.Log(""">>> [x] P#TCH#S #PPL##D""");
+    }
+
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VELDDev.BackroomsRenewed;
+
+public static class PluginInfo
+{
+    public const string PluginName = "Backrooms Renewed";
+    public const string GUID = "VELDDev.BackroomsRenewed";
+    public const string Author = "VELD-Dev";
+    public const string Version = "1.0.0";
+}
